cd6a3e5467a8b86b2bd773d2612c923ee41e376d
foreverreachingfordistantstars <mantuladani@proton.me>
2026-03-28 16:13:16 -0500

    User description added
    The user added a brief introduction of themselves to the list of text to be printed when this project is run.

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,6 @@ def main():
     name = "Daniela Mantula" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
-
+    print(f"Hey!! ","I'm ",{name}, "a junior from Dallas International School (originally, I am from Kyiv, but I currently reside in Texas)! One fun fact about me is that I'm a huge spacecraft systems nerd and I love to design 3D model, you guessed it, spacecraft! I'm also part of my school's aerial robotics team (we compete in the National ARISE drone competition yearly, and this year I'm working on a set of PCBs for the video system), and the lead Science and Tech editor in my school newspaper, as well as the founder of a legislative debate club based partially on my experiences in Youth and Government. I'm really glad to have the chance to participate in this course (and UAS-SAR, the course I'm taking this course for) and to meet you all, and I hope to see you in the summer (whether virtually or in person)!")
 if __name__ == "__main__":
     main()
